198ed50279a7aea3bdabeb0d32a82c93d9e6df89
bartek <wbkaczorowski@gmail.com>
2013-05-06 01:39:53 +0200

    Improved way of keeping nodes in new gragh. TODO calculation dQ, weighted wdges, selection of partition.

--- Library/src/pl/edu/pw/elka/community/finding/algorithms/Louvain.java
+++ Library/src/pl/edu/pw/elka/community/finding/algorithms/Louvain.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Phaser;
 
@@ -19,6 +20,7 @@ public class Louvain<V, E> implements Algorithm<V, E> {
 	private HashSet<Set<V>> mainGroups;
 	private HashSet<Set<V>> tmpGroups;
 	private Transformer<V, Set<V>> moduleMembership;
+	private final double epsilon = 0.0001;
 
 	public Louvain() {
 		moduleMembership = new Transformer<V, Set<V>>() {
@@ -33,14 +35,14 @@ public class Louvain<V, E> implements Algorithm<V, E> {
 	@Override
 	public Set<Set<V>> getCommunities(Graph<V, E> graph) {
 		mainGroups = new HashSet<>();
-
 		Graph<V, E> loopGraph = graph;
 
 		while (true) {
-			if(!phaseOne(loopGraph)) {
+			if (!phaseOne(loopGraph)) {
+				System.out.println("Warunek stopu osiągnięty");
 				break;
 			}
-			
+
 			loopGraph = phaseTwo(loopGraph);
 
 			System.out.println("Nowy graf");
@@ -51,6 +53,10 @@ public class Louvain<V, E> implements Algorithm<V, E> {
 			for (E e : loopGraph.getEdges()) {
 				System.out.println("----" + e + " " + loopGraph.getEndpoints(e));
 			}
+		}
+
+		for (V g : loopGraph.getVertices()) {
+			mainGroups.add((Set<V>) g);
 		}
 
 		for (Set<V> g : mainGroups) {
@@ -64,12 +70,12 @@ public class Louvain<V, E> implements Algorithm<V, E> {
 	/*
 	 * Phase I of algorithm. Connecting nodes, in initials modules.
 	 */
-	
+
 	private boolean phaseOne(Graph<V, E> graph) {
 		boolean gainOccurred = false;
 		double maxDQ = 0;
 		tmpGroups = new HashSet<>();
-		
+
 		/*
 		 * Setting every node in different group.
 		 */
@@ -78,57 +84,62 @@ public class Louvain<V, E> implements Algorithm<V, E> {
 			group.add(v);
 			tmpGroups.add(group);
 		}
-		
-		
+
+		// if (tmpGroups.size() <= 2) {
+		// return gainOccurred;
+		// }
+
 		for (V i : graph.getVertices()) {
 			maxDQ = 0;
 			for (V neighbor : graph.getNeighbors(i)) {
 
-				HashSet<V> vertexGroup = (HashSet<V>) findVertexGroup(i);
+				HashSet<V> originalGroup = (HashSet<V>) findVertexGroup(i);
 				double Q = calcualteModualarity(graph);
-				// System.out.println("Usuwam: " + i + " z grupy " + findVertexGroup(i));
+				System.out.println(Q);
+				System.out.println("Usuwam: " + i + " z grupy " + findVertexGroup(i));
 				findVertexGroup(i).remove(i);
-				// System.out.println("Dodaje do grupy: " + findVertexGroup(neighbor));
+				System.out.println("Dodaje do grupy: " + findVertexGroup(neighbor));
 				findVertexGroup(neighbor).add(i);
 				// TODO dlaczego moje nie działa?
 				// double dQ = calcualteDeltaModularity(i, findVertexGroup(neighbor), graph);
-
-				double dQ = calcualteModualarity(graph) - Q;
-				// System.out.println("dQ: " + dQ);
+				System.out.println(calcualteModualarity(graph));
+				double dQ = (double) Math.round((calcualteModualarity(graph) - Q) * 100000000) / 100000000; // TODO mało elegancko
+//				double dQ = (calcualteModualarity(graph) - Q);
+				System.out.println("dQ: " + dQ);
 
 				// set previous graph and groups topology if modularity difference is less than before modification
-				if (dQ < maxDQ) { // no gain of modularity
-					// System.out.println(" Powrót do poprzedniego stanu");
+				if (Math.abs(dQ) <= epsilon && dQ <= maxDQ) { // no gain of modularity
+					System.out.println(" Powrót do poprzedniego stanu");
 					findVertexGroup(i).remove(i);
-					vertexGroup.add(i);
-					// System.out.println("	Mamy grupy:");
-					// for (Set<V> g : groups) {
-					// System.out.println("		" + g);
-					// }
+					originalGroup.add(i);
+					System.out.println("	Mamy grupy:");
+					for (Set<V> g : tmpGroups) {
+						System.out.println("		" + g);
+					}
 				} else { // there is a gain of modularity
-					// System.out.println(" Zmiana zostaje");
+					System.out.println(" Zmiana zostaje");
 					maxDQ = dQ;
 					gainOccurred = true;
 				}
 				System.out.println("maxDQ: " + maxDQ);
 			}
 		}
-		
-		
-		
+
 		return gainOccurred;
 	}
 
 	/**
 	 * Phase II of algorithm, creating a temporary new graph based on previous groups.
+	 * 
 	 * @param graph
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	private Graph<V, E> phaseTwo(Graph<V, E> graph) {
-		Graph<String, String> newGraph = new UndirectedSparseGraph<>();
+		Graph<Set<V>, Integer> newGraph = new UndirectedSparseGraph<>();
 		for (Set<V> g : tmpGroups) {
 			if (!g.isEmpty()) {
-				newGraph.addVertex(g.toString());
+				newGraph.addVertex(g);
 			}
 		}
 		int edgeId = 0;
@@ -136,8 +147,7 @@ public class Louvain<V, E> implements Algorithm<V, E> {
 			if (!g.isEmpty()) {
 				for (E e : findOutgoingEdges(graph, g)) {
 					try {
-						newGraph.addEdge(String.valueOf(edgeId++), findVertexGroup(graph.getEndpoints(e).getFirst()).toString(),
-								findVertexGroup(graph.getEndpoints(e).getSecond()).toString());
+						newGraph.addEdge(edgeId++, findVertexGroup(graph.getEndpoints(e).getFirst()), findVertexGroup(graph.getEndpoints(e).getSecond()));
 					} catch (Throwable t) {
 						// TODO
 					}
@@ -184,6 +194,7 @@ public class Louvain<V, E> implements Algorithm<V, E> {
 	}
 
 	private double calcualteModualarity(Graph<V, E> graph) {
+//		return (double) Math.round(Modularity.computeScaledModularity(graph, moduleMembership) * 100000000) / 100000000;
 		return Modularity.computeScaledModularity(graph, moduleMembership);
 	}
 
